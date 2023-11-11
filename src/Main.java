d1b0864fcab38bc017ad4a9b1ec787047f508cdb
Ksenia Poshekhova <kposehova@gmail.com>
2023-11-11 20:18:29 +0300

    Inichial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int replenishmentAmount = 1100;
+        // int persent = 11;
+        int bonys = 11;
+
+        System.out.println("Общая сумма на счету клиента с учётом бонусных рублей составляет :");
+        System.out.println(amount + replenishmentAmount + bonys);
+    }
+}
