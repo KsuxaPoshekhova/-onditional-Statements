b3791149a8319c13857d487c6ed8da0bacd32505
Ksenia Poshekhova <kposehova@gmail.com>
2023-11-12 21:43:02 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 100;
-        int replenishmentAmount = 1100;
-        // int persent = 11;
-        int bonys = 11;
 
-        System.out.println("Общая сумма на счету клиента с учётом бонусных рублей составляет :");
-        System.out.println(amount + replenishmentAmount + bonys);
+        int amount = 100;
+        int contribution = 1100;
+        int invest = 100;
+        int percent;
+        percent = 11;
+
+        boolean facilities = contribution > invest;
+        if (contribution > invest) {
+            System.out.println("Итоговая сумма на счету клиента составляет :");
+            System.out.println(amount + contribution + percent);
+        } else {
+            System.out.println("Бонусов нет");
+            System.out.println(amount + invest);
+        }
     }
 }
