cc383c9e34a3087d2bf86312914c66bc703c1053
Ksenia Poshekhova <kposehova@gmail.com>
2023-11-13 20:35:58 +0300

    Komut

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 100;
-        int contribution = 1000;
+        int contribution = 1100;
         int percent = 1;
-        int bonys = contribution == contribution  ? (contribution / amount ) : (contribution - amount );
+        int bonys = contribution == contribution ? (contribution / amount) : (contribution - amount);
         System.out.println("Количество бонусов составляет :");
-        System.out.println( bonys  );
+        System.out.println(bonys);
 
-        boolean facilities = contribution > amount ;
-        if (contribution > amount ) {
-            System.out.println("Итоговая сумма на счету клиента составляет :");
-            System.out.println(amount + contribution + bonys  );
-        } else {
-            System.out.println("Бонусов нет");
-            System.out.println(amount + amount );
-        }
+        System.out.println("Итоговая сумма на счету клиента составляет :");
+        System.out.println(amount + contribution + bonys);
     }
 }
